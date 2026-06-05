117acb94b7e93e842f25d28d80b8b0a37bb91f12
alberto8812 <cvelascosaavedra@gmail.com>
2026-06-05 17:38:46 -0500

    feat: load poppins font

--- src/shared/pdf/fonts.ts
+++ src/shared/pdf/fonts.ts
@@ -1,7 +1,5 @@
 import { Font } from '@react-pdf/renderer';
 
-const BASE = 'https://cdn.jsdelivr.net/npm/@fontsource/poppins@5.1.0/files';
-
-Font.register({ family: 'Poppins', src: `${BASE}/poppins-latin-400-normal.woff2` });
-Font.register({ family: 'Poppins-Bold', src: `${BASE}/poppins-latin-700-normal.woff2` });
-Font.register({ family: 'Poppins-Italic', src: `${BASE}/poppins-latin-400-italic.woff2` });
+Font.register({ family: 'Poppins', src: '/fonts/Poppins-Regular.ttf' });
+Font.register({ family: 'Poppins-Bold', src: '/fonts/Poppins-Bold.ttf' });
+Font.register({ family: 'Poppins-Italic', src: '/fonts/Poppins-Italic.ttf' });
